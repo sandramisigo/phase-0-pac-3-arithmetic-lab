da02358d9908a3c419d198f50e6b0bed6c1bc16c
Sandra Misigo <misigosandra@gmail.com>
2024-11-19 23:58:00 +0300

    arithmetic lab

---
+++ index.js
@@ -0,0 +1,39 @@
+function add(a, b) { 
+    return a + b;
+    // we'll fill this in shortly
+  }
+
+ function subtract(a,b) {
+    return a-b;
+
+ }
+
+ function multiply(a,b){
+    return a*b;
+
+ }
+ function divide(a,b) {
+    return a/b;
+ }
+
+ function increment(number){
+   return number+=1;
+ }
+
+ function decrement(number){
+    return number-=1;
+ }
+
+ function makeInt(number){
+   return parseInt(number,10);
+ }
+
+ function preserveDecimal(number) {
+    return parseFloat(number);
+
+ }
+
+ 
+
+
+
